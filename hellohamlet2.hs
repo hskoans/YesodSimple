bf19c465a7a92287b59807564f8675fb1a270e77
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-12 20:31:39 +0800

    hamlet template with embedded hamlet template and a type-safe url in the embedded hamlet template

---
+++ hellohamlet2.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude
+import Text.Hamlet (HtmlUrl, hamlet)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Data.Text (Text)
+
+data MyRoute = Home
+
+render :: MyRoute -> [(Text, Text)] -> Text
+render Home _ = "/home"
+
+footer :: HtmlUrl MyRoute
+footer = [hamlet|
+    <footer>
+        Return to #
+        <a href=@{Home}>Homepage
+        .
+|]
+
+main :: IO()
+main = putStrLn $ renderHtml $ [hamlet|
+    <body>
+        <p>This is my page
+        ^{footer}
+|] render
