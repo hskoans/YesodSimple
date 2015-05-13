fdd3743283394fe1017e437ff50a6d6ee92cdcb4
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-13 09:24:01 +0800

    Using   and  in hamlet

--- Main.hs
+++ Main.hs
@@ -17,20 +17,16 @@ mkYesod "App" [parseRoutes|
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-    let filename1 = "readme.txt" :: String
-        filename2 = "report.pdf" :: String
-        filename3 = "music.wav" :: String
+    let filenames = [] :: [String]
     setTitle "File Processor"
     toWidget [whamlet|
 <h2>Previously submitted files
-<ul>
-$# demarcates a commented line in hamlet
-$#  <li>readme.txt
-$#  <li>report.pdf
-$#  <li>music.wav
-    <li>#{filename1}
-    <li>#{filename2}
-    <li>#{filename3}
+$if null filenames
+    <p>No files have been uploaded yet.
+$else
+    <ul>
+        $forall filename <- filenames
+            <li>#{filename}
 |]
 
 main :: IO()
