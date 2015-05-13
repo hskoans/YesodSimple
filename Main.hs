ebce5fd958ba755f09886a8e524046fb986e864d
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-13 11:44:00 +0800

    Separating out the routes into config/routes2 and separating the view into templates/home2.hamlet

--- Main.hs
+++ Main.hs
@@ -7,27 +7,19 @@ module Main where
 
 import Prelude
 import Yesod
+import Data.Default
+import Yesod.Default.Util
 
 data App = App
 instance Yesod App
 
-mkYesod "App" [parseRoutes|
-/ HomeR GET
-|]
+mkYesod "App" $(parseRoutesFile "config/routes2")
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-    let filenames = ["hello.txt", "whatever.png"] :: [String]
+    let filenames = ["hello.txt", "whatever.png", "hooray.jpg"] :: [String]
     setTitle "File Processor"
-    toWidget [whamlet|
-<h2>Previously submitted files
-$if null filenames
-    <p>No files have been uploaded yet.
-$else
-    <ul>
-        $forall filename <- filenames
-            <li>#{filename}
-|]
+    $(widgetFileNoReload def "home2")
 
 main :: IO()
 main = warpEnv App
