89dfb4f7e9136f3648a5d2082f6c35def1a32790
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-13 16:26:10 +0800

    Using Control.Concurrent.STM functions to mutate our global TVar atomically

--- Foundation2.hs
+++ Foundation2.hs
@@ -7,13 +7,20 @@ module Foundation2 where
 import Prelude
 import Yesod
 import Data.Text (Text)
+import Control.Concurrent.STM
 
-data App = App [Text] -- Using Text to store our filenames at application state level
+data App = App (TVar [Text])    -- Using Text to store our filenames at application state level
+                                -- Use TVar to mark our Text list as a transactional variable
 instance Yesod App
 
 mkYesodData "App" $(parseRoutesFile "config/routes2")
 
 getList :: Handler [Text]
 getList = do
-    App state <- getYesod
-    return state
+    App tstate <- getYesod
+    liftIO $ readTVarIO tstate
+
+addFile :: App -> Text -> Handler ()
+addFile (App tstore) op =
+    liftIO . atomically $ do
+        modifyTVar tstore $ \ ops -> op : ops
--- Main.hs
+++ Main.hs
@@ -5,8 +5,12 @@ module Main where
 import Prelude
 import Yesod
 
+import Control.Concurrent.STM
+
 import Dispatch2 () -- this means that the only thing we want to import from Dispatch2.hs is its Yesod class instance
 import Foundation2
 
 main :: IO ()
-main = warpEnv $ App ["readme.txt", "report.pdf", "music.wav"]
+main = do
+    tfilenames <- atomically $ newTVar ["readme.txt", "report.pdf", "music.wav"]
+    warpEnv $ App tfilenames 
